9f74d921ecb4ad740ad0a09c6e3f606e2ac11934
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-23 16:50:18 +0100

    adds confirmation page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 // import { Welcome } from './src/pages/Welcome';
-import { UserIdentification } from './src/pages/UserIdentification';
+// import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 import {
   useFonts,
   Jost_400Regular,
@@ -16,5 +17,5 @@ export default function App() {
 
   if(!fontsLoaded) { return <AppLoading /> }
 
-  return <UserIdentification />
+  return <Confirmation />
 }
---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Button } from '../components/Button';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Confirmation() {
+  return (
+    <SafeAreaView style={ styles.container }>
+      <View style={ styles.content }>
+        <Text style={ styles.emoji }>
+          😀
+        </Text>
+        <Text style={ styles.title }>
+          Feito!
+        </Text>
+        <Text style={ styles.subtitle }>
+          Está na hora de tratar das suas plantas com todo o cuidado que elas merecem
+        </Text>
+        
+        <View style={ styles.footer }>
+          <Button title="Continuar" />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content : {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 30,
+  },
+  emoji : {
+    fontSize: 44,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingVertical: 20,
+    color: colors.heading,
+  }, 
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20,
+  },
+});
