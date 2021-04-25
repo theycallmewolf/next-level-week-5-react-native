414d6a6ce17928c34d05d62e548469efc3c446ac
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-25 10:47:15 +0100

    lists environment buttons from api

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -7,11 +7,35 @@ import {
 } from 'react-native';
 import { EnvironmentButton } from '../components/EnvironmentButton';
 
+import api from '../services/api';
+
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+
 export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>();
+  
+  useEffect(() => {
+    // function to use async / await
+    async function fetchEnvironment() {
+      const { data } = await api.get('plants_environments');
+      setEnvironments([
+        {
+          key: 'all',
+          title: 'Todos',
+        },
+        ...data
+      ]);
+    }
+    fetchEnvironment();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -23,8 +47,8 @@ export function PlantSelect() {
 
       <View>
         <FlatList 
-          data={[1,2, 3, 4, 5]}
-          renderItem={({item}) => <EnvironmentButton title="Cozinha"/> }
+          data={environments}
+          renderItem={({item}) => <EnvironmentButton title={item.title}/> }
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.environmentList}
