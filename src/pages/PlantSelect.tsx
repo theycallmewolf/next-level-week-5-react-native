10290a568fd350fe599ad379104efa7521f0d012
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-25 18:40:25 +0100

    adds key extractor to flatlists on plant select

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -119,8 +119,9 @@ export function PlantSelect() {
       </View>
 
       <View>
-        <FlatList 
+        <FlatList
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({item}) => (
             <EnvironmentButton
               title={item.title}
@@ -137,6 +138,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({item}) => <PlantCardPrimary data={item} />}
           showsVerticalScrollIndicator={false}
           numColumns={2}
