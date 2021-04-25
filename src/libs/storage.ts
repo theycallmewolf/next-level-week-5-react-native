5613c1476a042e322049f5a79af552c43d4f5f68
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-25 20:24:53 +0100

    adds save and load plants methods

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -14,3 +14,57 @@ export interface PlantProps {
   },
   dateTimeNotification: Date;
 }
+
+interface StoragePlantProps {
+  [id: string]: {
+    data: PlantProps,
+  }
+}
+
+export async function savePlant(plant : PlantProps) : Promise<void> {
+  try {
+    const data = await AsyncStorage.getItem('@plant_manager:plants');
+    const oldPlants = data ? ( JSON.parse(data) as StoragePlantProps) : {};
+    const newPlant = {
+      [plant.id] : {
+        data: plant
+      }
+    }
+
+    await AsyncStorage.setItem('@plant_manager:plants', 
+      JSON.stringify({
+        ...newPlant,
+        ...oldPlants,
+      })
+    )
+
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+export async function loadPlant() : Promise<PlantProps[]> {
+  try {
+    const data = await AsyncStorage.getItem('@plant_manager:plants');
+    const plants = data ? ( JSON.parse(data) as StoragePlantProps) : {};
+    const sortedPlants = Object
+      .keys(plants)
+      .map((plant) => {
+        return {
+          ...plants[plant].data,
+          hour: format(new Date(plants[plant].data.dateTimeNotification), 'HH:mm')
+        }
+      })
+      .sort((a, b) => 
+        Math.floor(
+          new Date(a.dateTimeNotification).getTime() / 1000 - 
+          Math.floor(new Date(b.dateTimeNotification).getTime() / 1000)
+        )
+      )
+    
+      return sortedPlants;
+    
+  } catch(error) {
+    throw new Error(error)
+  }
+}
