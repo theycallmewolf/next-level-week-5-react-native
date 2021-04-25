5744f91386f5b60ee866e734f031abdd90051bac
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-25 19:22:13 +0100

    adds plant save styles

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -9,11 +9,14 @@ import {
   Platform,
   TouchableOpacity,
 } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg';
 
 import waterdropIcon from '../assets/waterdrop.png';
 import { Button } from '../components/Button';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSave() {
   return (
@@ -57,12 +60,60 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     backgroundColor: colors.shape
   },
-  plantInfo: {},
-  plantName: {},
-  plantAbout: {},
-  controller: {},
-  tipContainer: {},
-  tipImage: {},
-  tipText: {},
-  alertLabel: {},
+  plantInfo: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.shape,
+  },
+  plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15
+  },
+  plantAbout: {
+    textAlign: 'center',
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 16,
+    marginTop: 10,
+  },
+  controller: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20,
+  },
+  tipContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    backgroundColor: colors.blue_light,
+    padding: 20,
+    borderRadius: 20,
+    marginTop: -60,
+    // position: 'relative',
+    // bottom: 60,
+  },
+  tipImage: {
+    width: 56,
+    height: 56,
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 16,
+  },
+  alertLabel: {
+    textAlign: 'center',
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+    marginBottom: 5,
+  },
 });
