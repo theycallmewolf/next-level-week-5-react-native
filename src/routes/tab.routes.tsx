5eb087febb56ae4cd0726e5a9bfa9bcafc0551d6
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-25 23:30:27 +0100

    fix tab navigation padding on android

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import colors from '../styles/colors';
+
 import { PlantSelect } from '../pages/PlantSelect';
 import { MaterialIcons } from '@expo/vector-icons';
 import { MyPlants } from '../pages/MyPlants';
+
+import colors from '../styles/colors';
 
 const AppTab = createBottomTabNavigator();
 
@@ -15,7 +18,7 @@ const AuthRoutes = () => {
         inactiveTintColor: colors.heading,
         labelPosition: 'beside-icon',
         style: {
-          paddingVertical: 20,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
           height: 88,
         }
       }}>
